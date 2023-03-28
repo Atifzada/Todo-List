1e565aa3a53e70257741b8170bb53d4bf7032b0b
Atifzada <116461959+Atifzada@users.noreply.github.com>
2023-03-29 03:51:11 +0500

    Clear linter errors

--- src/modules/renderTaskList.js
+++ src/modules/renderTaskList.js
@@ -1,11 +1,8 @@
 const renderTaskList = (Tasks, Element) => {
-  let innerHtml = Tasks.map((Tasks) => {
-    return `<li><div><input type="checkbox"/><p>${Tasks.description}</p></div>
-    <i class="material-icons dots">more_vert</i></i>`;
-  }).join('');
+  const innerHtml = Tasks.map((Tasks) => `<li><div><input type="checkbox"/><p>${Tasks.description}</p></div>
+    <i class="material-icons dots">more_vert</i></i>`).join('');
 
   Element.innerHTML = innerHtml;
 };
 
 export default renderTaskList;
-
